6d2567e77121860ae992416145a25b79861aa4b9
Ömer Köse <omerkose138@gmail.com>
2022-02-23 14:36:59 +0300

    Add project files.

---
+++ ProceduralGeneration/main.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+
+//My headers
+#include "progen/Utilities.h"
+#include "progen/Shader.h"
+#include "progen/Camera.h"
+
+
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+
+
+//Time parameters
+double deltaTime = 0.0;
+double lastFrame = 0.0;
+
+//Window
+GLFWwindow* window;
+
+
+void updateDeltaTime()
+{
+	double currentFrame = glfwGetTime();
+	deltaTime = currentFrame - lastFrame;
+	lastFrame = currentFrame;
+}
+
+//Callback function in case of resizing the window
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+//Function that will process the inputs, such as keyboard inputs
+void processInput(GLFWwindow* window)
+{
+	//If pressed glfwGetKey return GLFW_PRESS, if not it returns GLFW_RELEASE
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+
+	//Camera movement
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.processKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.processKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.processKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.processKeyboard(RIGHT, deltaTime);
+
+}
+
+//Callback function for mouse position inputs
+void mouse_callback(GLFWwindow* window, double xPos, double yPos)
+{
+	camera.processMouseMovement(xPos, yPos, GL_TRUE);
+}
+
+void scroll_callback(GLFWwindow* window, double xOffset, double yOffset)
+{
+	camera.processMouseScroll(yOffset);
+}
+
+
+int setup()
+{
+	glfwInit();
+	//Specify the version and the OpenGL profile. We are using version 3.3
+	//Note that these functions set features for the next call of glfwCreateWindow
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//Create the window object
+	window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL Window", NULL, NULL);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create the window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	//Before calling any OpenGL function we need to initialize GLAD since it manages the function pointers
+	//for OpenGL, which are OS-Specific.
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	//Specify the actual window rectangle for renderings.
+	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+
+	//Register our size callback funtion to GLFW.
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	//GLFW will capture the mouse and will hide the cursor
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	//Configure Global OpenGL State
+	glEnable(GL_DEPTH_TEST);
+
+	return 0;
+}
+
+
+int main()
+{
+	setup();
+
+	// render loop
+	// -----------
+	while (!glfwWindowShouldClose(window))
+	{
+		// input
+		// -----
+		processInput(window);
+
+		// render
+		// ------
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		// -------------------------------------------------------------------------------
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// glfw: terminate, clearing all previously allocated GLFW resources.
+	// ------------------------------------------------------------------
+	glfwTerminate();
+
+	return 0;
+}
