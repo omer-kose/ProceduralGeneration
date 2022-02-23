1a7d0bb950fc53775e8f62675a3451d48b5f18f7
Ömer Köse <omerkose138@gmail.com>
2022-02-23 15:52:40 +0300

    Project startup. -Librarires added. -Tested.

--- ProceduralGeneration/main.cpp
+++ ProceduralGeneration/main.cpp
@@ -114,9 +114,63 @@ int setup()
 }
 
 
+
+GLuint testRectangle()
+{
+	GLfloat vertices[] = 
+	{
+		 0.5f,  0.5f, 0.0f,  // top right
+		 0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  // bottom left
+		-0.5f,  0.5f, 0.0f   // top left 
+	};
+
+	GLuint indices[] = 
+	{  
+		2, 1, 0,  // first Triangle
+		2, 0, 3   // second Triangle
+	};
+
+	GLuint VAO, VBO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	//Bind VAO
+	glBindVertexArray(VAO);
+	//Bind VBO, send data
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	//Bind EBO, send indices 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	
+	//Configure Vertex Attributes
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+
+	//Data passing and configuration is done 
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	return VAO;
+}
+
+void renderTestRectangle(GLuint VAO, Shader shader)
+{
+	shader.use();
+	glBindVertexArray(VAO);
+	//total 6 indices since we have triangles
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+}
+
 int main()
 {
 	setup();
+
+	GLuint VAO = testRectangle();
+	Shader testShader("../Shaders/solidColor/solidColor.vs", "../Shaders/solidColor/solidColor.fs");
 
 	// render loop
 	// -----------
@@ -128,8 +182,13 @@ int main()
 
 		// render
 		// ------
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+		//Render Shapes
+		renderTestRectangle(VAO, testShader);
+
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
